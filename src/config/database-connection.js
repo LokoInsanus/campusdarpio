584b6a5ef3c338b052b20a405224ab3051708c65
Marcelo De Oliveira Costa Pereira <20221SI032@cefetes.br>
2025-04-04 11:04:25 -0300

    collection

--- src/config/database-connection.js
+++ src/config/database-connection.js
@@ -39,24 +39,22 @@ Pedido.associate(sequelize.models);
 Refeicao.associate(sequelize.models);
 TipoDeRefeicao.associate(sequelize.models);
 
-// databaseInserts();
+databaseInserts();
 
-function databaseInserts() {
-    (async () => {
-        await sequelize.sync({ force: true });
+function databaseInserts() {   
+    sequelize.sync({ force: true });
 
-        await Bebida.create({ nome: 'Coca-Cola', preco: 5.0 });
-        await Bloco.create({ nome: 'Bloco A' });
-        await Campus.create({ nome: 'Campus Central', endereco: 'Rua Principal, 123' });
-        await Cardapio.create({ data: new Date(), descricao: 'Cardápio do dia' });
-        await Cliente.create({ nome: 'João Silva', email: 'joao.silva@example.com', telefone: '123456789' });
-        await Entrega.create({ endereco: 'Rua Secundária, 456', status: 'Pendente' });
-        await Entregador.create({ nome: 'Carlos Souza', veiculo: 'Moto' });
-        await Funcionario.create({ nome: 'Maria Oliveira', cargo: 'Cozinheira' });
-        await Pedido.create({ clienteId: 1, total: 50.0, status: 'Em andamento' });
-        await Refeicao.create({ nome: 'Arroz com Feijão', preco: 10.0 });
-        await TipoDeRefeicao.create({ descricao: 'Vegetariana' });
-    })
+    // Bebida.create({ nome: 'Coca-Cola', tipo: 'Refrigerante', preco: 5.0, quantidade: 100 });
+    // Bloco.create({ nome: 'Bloco A' });
+    // Campus.create({ nome: 'Campus Central', endereco: 'Rua Principal, 123' });
+    // Cardapio.create({ data: new Date(), descricao: 'Cardápio do dia', campusId: 1 });
+    // Cliente.create({ nome: 'João Silva', email: 'joao.silva@example.com', telefone: '123456789' });
+    // Entrega.create({ endereco: 'Rua Secundária, 456', status: 'Pendente', pedidoId: 1 });
+    // Entregador.create({ nome: 'Carlos Souza', veiculo: 'Moto', status: 'Disponível', entregaId: 1 });
+    // Funcionario.create({ nome: 'Maria Oliveira', cargo: 'Cozinheira', status: 'Ativo', campusId: 1 });
+    // Pedido.create({ clienteId: 1, total: 50.0, status: 'Em andamento', cardapioId: 1 });
+    // Refeicao.create({ nome: 'Arroz com Feijão', preco: 10.0, status: 'Disponível', tipoDeRefeicaoId: 1 });
+    // TipoDeRefeicao.create({ descricao: 'Vegetariana', status: 'Ativo' });
 }
 
 export default sequelize;
