bc017769944ac2c135656f66c0dcbdbacfb47388
Roger De Assis Tedesco <20221SI031@cefetes.br>
2025-04-11 11:19:06 -0300

    correcao tipoderefeicao

--- src/models/TipoDeRefeicao.js
+++ src/models/TipoDeRefeicao.js
@@ -11,21 +11,28 @@ class TipoDeRefeicao extends Model {
           isIn: {
             args: [["0", "1", "2", "3", "4", "5"]],
             msg: "Tipo da Refeição deve ser 0, 1, 2, 3, 4 ou 5!"
+          },
+          async tipoUnicoPorValor(value) {
+            const where = { tipo: value };
+            if (this.id) {
+              where.id = { [this.sequelize.Sequelize.Op.ne]: this.id };
+            }
+
+            const existe = await this.constructor.findOne({ where });
+            if (existe) {
+              throw new Error(`Já existe um Tipo de Refeição com o tipo ${value}.`);
+            }
           }
         }
       },
-    }, { 
-      sequelize, 
-      modelName: 'tipoderefeicao', 
+    }, {
+      sequelize,
+      modelName: 'tipoderefeicao',
       tableName: 'tiposderefeicoes'
     });
   }
 
   static associate(models) {
-    this.hasMany(models.Refeicao, {
-      foreignKey: 'tipoDeRefeicaoId',
-      as: 'refeicoes'
-    });
   }
 
 }
