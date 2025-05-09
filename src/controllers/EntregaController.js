25bb07e98e8d5c8b2140313e9e71718925b5efdb
Marcelo De Oliveira Costa Pereira <20221SI032@cefetes.br>
2025-05-09 10:44:13 -0300

    Entrega Controller

---
+++ src/controllers/EntregaController.js
@@ -0,0 +1,37 @@
+import { EntregaService } from "../services/EntregaService.js";
+
+class EntregaController {
+  
+  static async findAll(req, res, next) {
+    EntregaService.findAll()
+        .then(objs => res.json(objs))
+        .catch(next);
+  }
+
+  static async findByPk(req, res, next) {
+    EntregaService.findByPk(req)
+        .then(obj => res.json(obj))
+        .catch(next);
+  }
+
+  static async create(req, res, next) {
+    EntregaService.create(req)
+        .then(obj => res.json(obj))
+        .catch(next);
+  }
+
+  static async update(req, res, next) {
+    EntregaService.update(req)
+        .then(obj => res.json(obj))
+        .catch(next);
+  }
+
+  static async delete(req, res, next) {
+    EntregaService.delete(req)
+        .then(obj => res.json(obj))
+        .catch(next);
+  }
+
+}
+
+export { EntregaController };
