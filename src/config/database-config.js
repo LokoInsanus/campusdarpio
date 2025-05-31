a7c201031a556f30bbd327c764d66dd637105138
LokoInsanus <Marceloboo240303@gmail.com>
2025-05-30 21:14:14 -0300

    postgres

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -1,9 +1,41 @@
+// export const databaseConfig = {
+//   dialect: 'sqlite',
+//   storage: 'database.sqlite',
+//   define: {
+//     timestamps: true,
+//     freezeTableName: true,
+//     underscored: true
+//   }
+// };
+
 export const databaseConfig = {
-    dialect: 'sqlite',
-    storage: 'database.sqlite',
-    define: {
-      timestamps: true,
-      freezeTableName: true,
-      underscored: true
+  dialect: 'postgres',
+  host: 'dpg-d0sr89k9c44c73ff2p70-a.oregon-postgres.render.com',
+  username: 'campusdarpio_user',
+  password: '8hVzGJLuQjsmr9Pzaq8fgq5G8rVRC6fT',
+  database: 'campusdarpio',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-  };
+  },
+  define: {
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true
+  }
+};
+
+// export const databaseConfig = {
+//   dialect: 'postgres',
+//   host: 'postgresql://campusdarpio_user:8hVzGJLuQjsmr9Pzaq8fgq5G8rVRC6fT@dpg-d0sr89k9c44c73ff2p70-a/campusdarpio',
+//   username: 'campusdarpio_user',
+//   password: 'postgres',
+//   database: 'campusdarpio',
+//   define: {
+//     timestamps: true,
+//     freezeTableName: true,
+//     underscored: true
+//   }
+// };
